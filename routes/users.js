4954849faec30ba5b3e06c844315a9070f0f5406
JohnsonNYC <jkow95@gmail.com>
2021-01-08 03:19:12 -0500

    Created a Delete placeholder. Needs Authentication

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,32 @@ const router = express.Router();
 const User = require('../models/User')
 const bodyParser = require('body-parser')
 
-//Create Users
+// Create Users
 router.post('/users', bodyParser.json(),async(req,res)=>{
-    const user = new User(req.body)
-    try{
-        await user.save()
-        res.status(200).send(user)
-    }catch(e){
-        res.status(404).send(e)
-    }
+ const user = new User(req.body)
+ try{
+  await user.save()
+  res.status(200).send(user)
+ }catch(e){
+  res.status(404).send(e)
+ }
 })
-// Get Users 
+// Read Users 
 router.get('/users', async(req,res)=>{
-    const users = await User.findAll();
-    res.send(users)
+ const users = await User.findAll();
+ res.send(users)
+})
+
+// Update Users
+
+// Delete Users
+router.delete('/users/:id', bodyParser.json(),async(req,res) => {
+  await User.destroy({
+    where: {
+    id: req.params.id 
+    }
+  }).then(() => res.send('User Deleted')).catch(e => console.log(e))
+
 })
 
 module.exports = router 
